05378657bba77f8ff0292ff2a7d730d025d5a287
RajTheDevloper <rajubailappa5456@gmail.com>
2024-08-31 21:54:48 +0530

    modified files added

--- truffle-config.js
+++ truffle-config.js
@@ -45,7 +45,7 @@ module.exports = {
     //127.0.0.1:8545
     development: {
       host: "127.0.0.1",     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
 
